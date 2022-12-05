cc6ad10a28a6b2be017f9b6f8550ae0020965e5a
Macedo <gustavomacedo1366@hotmail.com>
2022-12-05 17:59:06 -0300

    05/12 *2

--- IA/src/ia/Tree.java
+++ IA/src/ia/Tree.java
@@ -13,40 +13,9 @@ import java.util.List;
  */
 public class Tree {
     private Node raiz;
-    
-    private static class Node {
-            Peca Elem;
-            Node Pai;
-            List<Node> Filhos;
+}
 
-            public Node(Peca valor) {
-                    Elem = valor;
-                    Pai = null;
-                    Filhos = new ArrayList<>();
-            }
-    }
-
-    public Tree(Peca elem) {
-            raiz = new Node(elem);
-    }
-    public void inserir(Peca elem, Peca paiStr) {
-            Node pai = pesquisa(paiStr, raiz);
-            if (pai != null) {
-                    Node novo = new Node(elem);
-                    novo.Pai = pai;
-                    pai.Filhos.add(novo);
-                    System.out.println(pai.Filhos);
-            }
-    }
-
-    private Node pesquisa(Peca e, Node r) {
-            if (r.Elem.equals(e))
-                    return r;
-            for (Node f : r.Filhos) {
-                    Node aux = pesquisa(e, f);
-                    if (aux != null)
-                            return aux;
-            }
-            return null;
-    }
+class Node {
+    private int conteudo;
+    private List<Node> filhos;
 }
